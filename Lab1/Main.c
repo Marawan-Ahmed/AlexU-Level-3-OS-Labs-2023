e9f93d9d5e3958c2409bf9f64cb1022c4b2a7f3a
Marawan-Ahmed <memoahmedrabee@yahoo.com>
2023-03-06 23:23:53 +0200

    working without export

--- Lab1/Main.c
+++ Lab1/Main.c
@@ -1,17 +1,52 @@
 #include<stdio.h>
 #include<sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include<sys/wait.h>
 
 
 #define MAX_STR_SIZE 100
 char user_input[MAX_STR_SIZE];
-char  command_arr[MAX_STR_SIZE][MAX_STR_SIZE];
+char *command_arr[MAX_STR_SIZE];//[MAX_STR_SIZE];
+int background_flag;
+int number_commands;
 
 void read_input(void);
 void parse_input(void);
 int is_builtin(void);
+void execute_command(void);
 
-void main(void){
+
+
+
+
+void builtin_cd(void)
+{
+    if ((number_commands==2) || !(strcmp(command_arr[1], "~"))){
+        chdir(getenv("HOME"));
+    }
+    else if (chdir(command_arr[1]) != 0) {
+        printf("error in cd\n");
+    }
+}
+
+void builtin_echo(void)
+{
+    if (command_arr[1][0] == '"')
+    for(int i = 1; i <= strlen(command_arr[1]); i++) {
+        if(command_arr[1][i] == '"') break;
+        printf("%c", command_arr[1][i]);
+    }
+    printf("%s, \n",getenv("x"));
+    return;
+}
+void builtin_export(void){
+    return;
+}
+
+
+int main(void){
     // do
     //     parse_input(read_input())
     //     evaluate_expression():
@@ -27,90 +62,95 @@ void main(void){
         read_input();
         parse_input ();
         if (is_builtin()){
-
+            // execute_shell_bultin();
+            if(!(strcmp(command_arr[0],"cd"))){
+                builtin_cd();
+            }
+            else if (!(strcmp(command_arr[0],"echo"))){
+                builtin_echo();
+            }
+            else if (!(strcmp(command_arr[0],"export"))){
+                builtin_export();
+            }
         }
+        else if(!(strcmp(command_arr[0],"exit"))){
+            return;
+        }
+
         else{
-            
+            execute_command();            
         }
-
-
     }
     while (1);
+    return 0;
 }
 void read_input(void){
     printf("Enter your command: \n");
-    // scanf("%s", user_input);
     fgets(user_input, MAX_STR_SIZE, stdin);
-    // printf("You wrote: %s\n", user_input);
+    user_input[strcspn(user_input, "\n")] = '\0';
 }
 
 
 void parse_input (void){
     int input_length = 0;
-    int number_commands = 0;
-    while((user_input[input_length] != '\0')){
-        int j = 0;
-        while((user_input[input_length] != ' ') && (user_input[input_length] != '\0') ){
-            command_arr[number_commands][j] = user_input[input_length];
-            j++;
-            input_length++;
+    number_commands = 0;
+    background_flag = 0;
+    char * temp = strtok (user_input," ");
+
+    while (temp != NULL)
+    {
+        if (!(strcmp(temp, "&"))){
+            background_flag = 1;
+            temp = strtok (NULL, " ");           
+            continue;
         }
-        command_arr[number_commands] [j] = '\0';
+        command_arr[number_commands] = temp;
+        temp = strtok (NULL, " ");
         number_commands++;
     }
-    // printf("Your command (%d) is:", number_commands);
-    for (int i = 0; i < (number_commands - 1); i++){
-        // command_arr[i] = command_arr[i];
-        // printf("%s", command_arr[i]);
-    }
-    // printf("%s.\n", command_arr[number_commands-1]);
-    // printf("Your command is: %s,", command_arr[0]);
-    // printf(" %s,", command_arr[1]);
-    // printf(" %s\n", command_arr[2]);
+    command_arr[number_commands++] = NULL;
 }
 
 int is_builtin(void){
-    printf("(%s)", command_arr[0]);
-    if((command_arr[0] == "cd\0")||(command_arr[0] == "echo")){
-        printf("built in\n");
+    if(!(strcmp(command_arr[0], "cd")) || !(strcmp(command_arr[0], "echo")) || !(strcmp(command_arr[0], "export"))){
         return 1;
     }
     else{
-        printf("not built in\n");
         return 0;
     }
 }
 
-// void execute_command(void){
-//     // child_id = fork()
-//     // if child:
-//     //     execvp(command parsed)
-//     //     print("Error)
-//     //     exit()
-//     // else if parent and foreground:
-// 	//  waitpid(child)
-//     int child_id = fork();
-//     if (child_id < 0){
-//         fprintf(stderr,"Fork Failed");
-//         exit();
-//     }
+void execute_command(void){
+    // child_id = fork()
+    // if child:
+    //     execvp(command parsed)
+    //     print("Error)
+    //     exit()
+    // else if parent and foreground:
+	//  waitpid(child)
+    pid_t status;
 
-//     else if (child_id == 0){
-//         /*child process*/
-//         char* argument_list[] = {"ls", "-l", NULL}; // NULL terminated array of char* strings
+    int child_id = fork();
+    if (child_id < 0){
+        fprintf(stderr,"Fork Failed");
+        exit(0);
+    }
 
-//         // Ok! Will execute the command "ls -l"
-//         execvp("ls", argument_list);
-//         printf("Error");
-//         exit();
-//     }
-
-// }
-
-// void parent_main(void){
-//     // register_child_signal(on_child_exit())
-//     // setup_environment()
-//     // shell()
-
-
-//     }
+    else if (child_id == 0){
+        /*child process*/
+        if(execvp(command_arr[0], command_arr)<0){
+            printf("Error\n");
+        }
+        exit(0);
+    }
+    else {
+        // waiting for child to terminate
+        if(!(background_flag)){
+            waitpid(child_id,NULL,0);
+        }
+        else{
+            signal(SIGCHLD,SIG_IGN);
+        }
+        return;
+    }
+}
