06166fec65dcfb4ea4254f449b676064dad2a11c
Marawan-Ahmed <memoahmedrabee@yahoo.com>
2023-03-14 01:30:23 +0200

    .

--- Lab1/Main.c
+++ Lab1/Main.c
@@ -5,10 +5,13 @@
 #include <string.h>
 #include<sys/wait.h>
 
+#include <time.h>
 
 #define MAX_STR_SIZE 100
 char user_input[MAX_STR_SIZE];
 char *command_arr[MAX_STR_SIZE];//[MAX_STR_SIZE];
+char command_parameter[MAX_STR_SIZE];
+
 int background_flag;
 int number_commands;
 
@@ -16,10 +19,13 @@ void read_input(void);
 void parse_input(void);
 int is_builtin(void);
 void execute_command(void);
+void signal_handler(int);
 
-
-
-
+void setup_environment(void){
+    char directory[MAX_STR_SIZE];
+    getcwd(directory,sizeof(directory));
+    chdir(directory);
+}
 
 void builtin_cd(void)
 {
@@ -33,36 +39,30 @@ void builtin_cd(void)
 
 void builtin_echo(void)
 {
-    if (command_arr[1][0] == '"')
-    for(int i = 1; i <= strlen(command_arr[1]); i++) {
-        if(command_arr[1][i] == '"') break;
-        printf("%c", command_arr[1][i]);
+    for(int i = 0; i <= strlen(command_arr[1]); i++) {
+        if (command_arr[1][i] == '"') continue;
+        printf("%c",command_arr[1][i]);
     }
-    printf("%s, \n",getenv("x"));
+    for(int j = 2; j < (number_commands-1); j++){
+        if (j == (number_commands -2)) command_arr[j][strlen(command_arr[j])-1] ='\0';
+        printf(" %s", command_arr[j]);
+    }
+    printf("\n");
     return;
 }
+
 void builtin_export(void){
+    putenv(strdup(command_parameter));
     return;
 }
 
 
 int main(void){
-    // do
-    //     parse_input(read_input())
-    //     evaluate_expression():
-    //     switch(input_type):
-    //         case shell_builtin:
-    //             execute_shell_bultin();
-    //         case executable_or_error:
-    //             execute_command():
-
-    // while command_is_not_exit
-
+    setup_environment();
     do{
         read_input();
         parse_input ();
         if (is_builtin()){
-            // execute_shell_bultin();
             if(!(strcmp(command_arr[0],"cd"))){
                 builtin_cd();
             }
@@ -78,25 +78,49 @@ int main(void){
         }
 
         else{
-            execute_command();            
+            execute_command();    
         }
     }
     while (1);
     return 0;
 }
-void read_input(void){
-    printf("Enter your command: \n");
-    fgets(user_input, MAX_STR_SIZE, stdin);
-    user_input[strcspn(user_input, "\n")] = '\0';
-}
 
+void read_input(void){
+    printf("Enter your command: ");
+    char input_str_buffer[MAX_STR_SIZE]; 
+    fgets(input_str_buffer, MAX_STR_SIZE, stdin);
+    input_str_buffer[strcspn(input_str_buffer, "\n")] = '\0';
+    int input_index;
+    int output_index = 0;
+
+    for (input_index = 0; input_index < strlen(input_str_buffer)+1;input_index++){
+        if (input_str_buffer[input_index] == '$'){
+            char temp [MAX_STR_SIZE];
+            temp[0] = input_str_buffer[input_index+1];
+            temp[1] = '\0';
+            strcpy(temp, getenv(temp));
+            for (int j = 0; j<strlen(temp); j++){
+                if (temp[j] !='"'){
+                    user_input[output_index] = temp[j];
+                    output_index++;
+                }
+            }
+            input_index++;
+        }
+        else{
+            user_input[output_index] = input_str_buffer[input_index];
+            output_index++;
+        }
+    }
+}
 
 void parse_input (void){
     int input_length = 0;
     number_commands = 0;
     background_flag = 0;
-    char * temp = strtok (user_input," ");
 
+    char * temp = strtok (user_input," ");
+    strcpy(command_parameter, user_input+strlen(temp)+1);
     while (temp != NULL)
     {
         if (!(strcmp(temp, "&"))){
@@ -121,13 +145,6 @@ int is_builtin(void){
 }
 
 void execute_command(void){
-    // child_id = fork()
-    // if child:
-    //     execvp(command parsed)
-    //     print("Error)
-    //     exit()
-    // else if parent and foreground:
-	//  waitpid(child)
     pid_t status;
 
     int child_id = fork();
@@ -149,8 +166,29 @@ void execute_command(void){
             waitpid(child_id,NULL,0);
         }
         else{
-            signal(SIGCHLD,SIG_IGN);
+            signal(SIGCHLD,signal_handler);
         }
         return;
     }
+}
+
+void signal_handler(int sig)
+{
+    while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) {}
+
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    FILE *f = fopen("/media/marawan/Disk/Documents/C programing/OS/OS/Lab1/file.txt", "a");
+    if (f == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    fprintf(f, "Child was closed in %s \n.........\n", asctime (timeinfo));
+
+    fclose(f);
 }
---
+++ Lab2/Main.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STR_SIZE 100
+
+void main (int argc, char **argv){
+    char input_file_1[MAX_STR_SIZE],input_file_1[MAX_STR_SIZE],output_file[MAX_STR_SIZE];
+    char matrix_1_row, matrix_1_column;
+    char matrix_2_row, matrix_2_column;
+
+    
+    return;
+}
+
